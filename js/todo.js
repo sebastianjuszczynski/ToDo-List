8d28ddbbd589edd5649121a29cf23dad38f3722e
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-06-21 19:31:23 +0100

    trying to fix function - removeTask

--- js/todo.js
+++ js/todo.js
@@ -5,11 +5,23 @@ const inputAdd = document.querySelector(".form__inputAdd");
 const inputSearch = document.querySelector(".form__inputSearch");
 
 const listElements = [...document.querySelectorAll("li")];
+const buttonElements = [...document.querySelectorAll(".task button")];
+
+// document.getElementById("e").addEventListener("click", function(e) {
+//   var index = Array.prototype.indexOf.call(this.parent, e.target);
+//   console.log(index);
+// });
 
 const removeTask = (e) => {
+  let btnActive = e.target.parentNode.textContent.toLowerCase();
+  console.log(btnActive);
+  let index = listElements.indexOf(btnActive);
+  
+  if (index > - 1) {
+  listElements.splice(index, 1);
   e.target.parentNode.remove();
-  listElements.pop(e.target.parentNode);
   mainSpan.textContent = listElements.length;
+  }
 };
 
 const addTask = (e) => {
@@ -17,10 +29,16 @@ const addTask = (e) => {
   const taskName = inputAdd.value;
   if (taskName === "") return;
   const task = document.createElement("li");
-  task.innerHTML = taskName + "<button class='remove'>-</button>";
+  task.className = "task";
+  task.innerHTML = taskName;
   list.appendChild(task);
+  const button = document.createElement("button");
+  button.className = "remove";
+  button.innerHTML = "";
+  task.appendChild(button);
   inputAdd.value = "";
   listElements.push(task);
+  buttonElements.push(button);
   mainSpan.textContent = listElements.length;
 
   task.querySelector("button").addEventListener("click", removeTask);
@@ -36,7 +54,7 @@ const searchTask = (e) => {
   );
   list.textContent = "";
   tasks.forEach((li) => list.appendChild(li));
-  mainSpan.textContent = tasksElements.length;
+  mainSpan.textContent = listElements.length;
 };
 
 inputSearch.addEventListener("input", searchTask);
